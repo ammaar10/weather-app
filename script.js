af09c45766a8bd1a8a89847e91efd3f12e73f88e
ammaar10 <nammaar1@gmail.com>
2026-05-30 20:50:55 +0530

    Connected OpenWeather API and fetched raw data

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.getElementById("search").addEventListener("click", () => {
+document.getElementById("search").addEventListener("click",async() =>{
     const city = document.getElementById("city").value;
-    console.log(city);
+
+    const apiKey = "8791758cc9dd64414722ff12702de42a";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data);
 })
+
